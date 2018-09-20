f2a06119e1645771f75f2d80c83f60bcf38a45eb
xnr_admin <chinesepython@163.com>
2018-09-20 17:15:27 +0800

    添加retweet功能

--- xnr/facebook_info/fb_tweet_func.py
+++ xnr/facebook_info/fb_tweet_func.py
@@ -3,18 +3,20 @@ import sys
 sys.path.append('/home/xnr_overseas/xnr/util_tools/')
 sys.path.append('/home/xnr_overseas/xnr_spider/facebook/')
 from fb_operate import Operation
-
+from send_mail import send_email_to
 
 
 
 # 发帖
 def fb_publish(account_name, password, text, tweet_type, xnr_user_no):
-    print '123'
     print 'account_name,password..',account_name,password
-    operation = Operation(account_name,password)
-    print 'operation...',operation
-    print operation.publish(text)
-    mark = True
+    try:
+        operation = Operation(account_name,password)
+        print 'operation...',operation
+        print operation.publish(text)
+        mark = True
+    except Exception as e:
+        send_email_to('80617252@qq.com', 'somethin occur in your project details is /home/xnr_overseas/xnr/facebookinfo/fb_tweet_func{}'.format(e))
     # except:
     #     mark = False
 
@@ -28,3 +30,14 @@ def fb_publish(account_name, password, text, tweet_type, xnr_user_no):
     #    save_mark = False
 
     return mark
+
+# 转发
+def fb_retweet(account_name, password, _id, uid, text, tweet_type, xnr_user_no):
+
+    operation = Operation(account_name,password)
+    _id = _id.split('_')[1]
+    print 'fid', _id, type(_id)
+    try:
+        print operation.share(uid, _id, text)
+    except Exception as e:
+        send_email_to('80617252@qq.com', 'somethin occur in your project details is /home/xnr_overseas/xnr/facebookinfo/fb_tweet_func{}'.format(e))
